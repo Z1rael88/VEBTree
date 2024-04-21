06bd339f056e5a3a2e8fd30e77f4cf7a15c114b3
Z1rael88 <oleksiibahmet@gmail.com>
2024-04-21 10:03:28 +0200

    refactored ,version 2.1

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,18 @@ class Van_Emde_Boas {
         minimum = -1;
         maximum = -1;
 
-        // Base case
         if (size <= 2) {
             summary = null;
-            clusters = new ArrayList<Van_Emde_Boas>(0);
+            clusters = new ArrayList<>(0);
         }
         else {
-            int no_clusters
-                    = (int)Math.ceil(Math.sqrt(size));
+            int no_clusters = (int)Math.ceil(Math.sqrt(size));
             summary = new Van_Emde_Boas(no_clusters);
 
-            clusters
-                    = new ArrayList<Van_Emde_Boas>(no_clusters);
+            clusters = new ArrayList<>(no_clusters);
 
             for (int i = 0; i < no_clusters; i++) {
-                clusters.add(new Van_Emde_Boas(
-                        (int)Math.ceil(Math.sqrt(size))));
+                clusters.add(new Van_Emde_Boas((int)Math.ceil(Math.sqrt(size))));
             }
         }
     }
@@ -51,8 +47,8 @@ class Van_Emde_Boas {
     // Function to return position of x in cluster
     public int generate_index(int x, int y)
     {
-        int ru = (int)Math.ceil(Math.sqrt(universe_size));
-        return x * ru + y;
+        int index = (int)Math.ceil(Math.sqrt(universe_size));
+        return x * index + y;
     }
 }
 
@@ -100,11 +96,8 @@ class Main {
 
                 // If no key is present in the cluster then
                 // insert key into both cluster and summary
-                if (VEB_minimum(helper.clusters.get(
-                        helper.high(key)))
-                        == -1) {
-                    insert(helper.summary,
-                            helper.high(key));
+                if (VEB_minimum(helper.clusters.get(helper.high(key))) == -1) {
+                    insert(helper.summary, helper.high(key));
 
                     // Sets the minimum and maximum of
                     // cluster to the key as no other keys
@@ -122,9 +115,7 @@ class Main {
                     // tree then recursively go deeper into
                     // the structure to set attributes
                     // accordingly
-                    insert(helper.clusters.get(
-                                    helper.high(key)),
-                            helper.low(key));
+                    insert(helper.clusters.get(helper.high(key)), helper.low(key));
                 }
             }
             // Sets the key as maximum it is greater than
@@ -143,8 +134,7 @@ class Main {
             return false;
         }
 
-        if (helper.minimum == key
-                || helper.maximum == key) {
+        if (helper.minimum == key || helper.maximum == key) {
             return true;
         }
         else {
@@ -155,9 +145,7 @@ class Main {
                 return false;
             }
             else {
-                return isMember(
-                        helper.clusters.get(helper.high(key)),
-                        helper.low(key));
+                return isMember(helper.clusters.get(helper.high(key)), helper.low(key));
             }
         }
     }
@@ -176,51 +164,42 @@ class Main {
         }
         // If key is less than minimum then return minimum
         // because it will be successor of the key
-        else if (helper.minimum != -1
-                && key < helper.minimum) {
+        else if (helper.minimum != -1 && key < helper.minimum) {
             return helper.minimum;
         }
         else {
 
             // Find successor inside the cluster of the key
             // First find the maximum in the cluster
-            int max_incluster = VEB_maximum(
-                    helper.clusters.get(helper.high(key)));
-            int offset = 0;
-            int succ_cluster = 0;
+            int max_incluster = VEB_maximum(helper.clusters.get(helper.high(key)));
+            int offset;
+            int succ_cluster;
 
             // If there is any key( maximum!=-1 ) present in
-            // the cluster then find the successor inside of
+            // the cluster then find the successor inside
             // the cluster
-            if (max_incluster != -1
-                    && helper.low(key) < max_incluster) {
-                offset = VEB_successor(
-                        helper.clusters.get(helper.high(key)),
-                        helper.low(key));
-                return helper.generate_index(
-                        helper.high(key), offset);
+            if (max_incluster != -1 && helper.low(key) < max_incluster)
+            {
+                offset = VEB_successor(helper.clusters.get(helper.high(key)), helper.low(key));
+
+                return helper.generate_index(helper.high(key), offset);
             }
             else {
-                succ_cluster = VEB_successor(
-                        helper.summary, helper.high(key));
+                succ_cluster = VEB_successor(helper.summary, helper.high(key));
                 if (succ_cluster == -1) {
                     return -1;
                 }
                 // Find minimum in successor cluster which
                 // will be the successor of the key
                 else {
-                    offset = VEB_minimum(
-                            helper.clusters.get(succ_cluster));
-                    return helper.generate_index(
-                            succ_cluster, offset);
+                    offset = VEB_minimum(helper.clusters.get(succ_cluster));
+
+                    return helper.generate_index(succ_cluster, offset);
                 }
             }
         }
     }
-
-    // Function to find the predecessor of the given key
-    public static int VEB_predecessor(Van_Emde_Boas helper,
-                                      int key)
+    public static int VEB_predecessor(Van_Emde_Boas helper, int key)
     {
         if (helper.universe_size == 2) {
             if (key == 1 && helper.minimum == 0) {
@@ -234,39 +213,33 @@ class Main {
         // then
         // return key as it will be the predecessor of the
         // key
-        else if (helper.maximum != -1
-                && key > helper.maximum) {
+        else if (helper.maximum != -1 && key > helper.maximum) {
             return helper.maximum;
         }
         else {
             // Find predecessor in the cluster of the key
             // First find minimum in the key to check
             // whether any key is present in the cluster
-            int min_incluster = VEB_minimum(
-                    helper.clusters.get(helper.high(key)));
-            int offset = 0;
-            int pred_cluster = 0;
+            int min_incluster = VEB_minimum(helper.clusters.get(helper.high(key)));
+            int offset;
+            int pred_cluster;
 
             // If any key is present in the cluster then
             // find predecessor in the cluster
             if (min_incluster != -1
                     && helper.low(key) > min_incluster) {
 
-                offset = VEB_predecessor(
-                        helper.clusters.get(helper.high(key)),
-                        helper.low(key));
-                return helper.generate_index(
-                        helper.high(key), offset);
+                offset = VEB_predecessor(helper.clusters.get(helper.high(key)), helper.low(key));
+
+                return helper.generate_index(helper.high(key), offset);
             }
             else {
                 // returns the index of predecessor cluster
                 // with any key present
-                pred_cluster = VEB_predecessor(
-                        helper.summary, helper.high(key));
+                pred_cluster = VEB_predecessor(helper.summary, helper.high(key));
                 // If no predecessor cluster then...
                 if (pred_cluster == -1) {
-                    if (helper.minimum != -1
-                            && key > helper.minimum) {
+                    if (helper.minimum != -1 && key > helper.minimum) {
                         return helper.minimum;
                     }
                     else {
@@ -275,16 +248,14 @@ class Main {
                 } // Otherwise find maximum in the
                 // predecessor cluster
                 else {
-                    offset = VEB_maximum(
-                            helper.clusters.get(pred_cluster));
-                    return helper.generate_index(
-                            pred_cluster, offset);
+                    offset = VEB_maximum(helper.clusters.get(pred_cluster));
+
+                    return helper.generate_index(pred_cluster, offset);
                 }
             }
         }
     }
-    public static void VEB_delete(Van_Emde_Boas helper,
-                                  int key)
+    public static void VEB_delete(Van_Emde_Boas helper, int key)
     {
         // If only one key is present, it means
         // that it is the key we want to delete
@@ -313,67 +284,44 @@ class Main {
             // bigger key and assign it as minimum
             if (key == helper.minimum) {
 
-                int first_cluster
-                        = VEB_minimum(helper.summary);
+                int first_cluster = VEB_minimum(helper.summary);
 
-                key = helper.generate_index(
-                        first_cluster,
-                        VEB_minimum(helper.clusters.get(
-                                first_cluster)));
-
+                key = helper.generate_index(first_cluster, VEB_minimum(helper.clusters.get(first_cluster)));
                 helper.minimum = key;
             }
 
             // Now we delete the key
-            VEB_delete(
-                    helper.clusters.get(helper.high(key)),
-                    helper.low(key));
-
-            // After deleting the key, rest of the
-            // improvements
+            VEB_delete(helper.clusters.get(helper.high(key)), helper.low(key));
 
             // If the minimum in the cluster of the key is
             // -1 then we have to delete it from the summary
             // to eliminate the key completely
             if (VEB_minimum(
-                    helper.clusters.get(helper.high(key)))
-                    == -1) {
+                    helper.clusters.get(helper.high(key))) == -1) {
 
-                VEB_delete(helper.summary,
-                        helper.high(key));
+                VEB_delete(helper.summary, helper.high(key));
 
                 // After the above condition, if the key
                 // is maximum of the tree then.
                 if (key == helper.maximum) {
-                    int max_insummary
-                            = VEB_maximum(helper.summary);
+                    int max_insummary = VEB_maximum(helper.summary);
 
                     if (max_insummary == -1) {
-
                         helper.maximum = helper.minimum;
                     }
                     else {
                         // Assign global maximum of the
                         // tree, after deleting our
                         // query-key
-                        helper.maximum
-                                = helper.generate_index(
-                                max_insummary,
-                                VEB_maximum(
-                                        helper.clusters.get(
-                                                max_insummary)));
+                        helper.maximum = helper.generate_index(max_insummary, VEB_maximum(helper.clusters.get(max_insummary)));
                     }
                 }
             }
-
             // Simply find the new maximum key and
             // set the maximum of the tree
             // to the new maximum
             else if (key == helper.maximum) {
-                helper.maximum = helper.generate_index(
-                        helper.high(key),
-                        VEB_maximum(helper.clusters.get(
-                                helper.high(key))));
+                helper.maximum = helper.generate_index(helper.high(key), VEB_maximum(helper.clusters.get(helper.high(key))));
             }
         }
     }
@@ -387,12 +335,11 @@ class Main {
         System.out.println("6. Exit");
         System.out.println("Enter your choice:");
     }
-
     // Driver code
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        Van_Emde_Boas end = new Van_Emde_Boas(8);
+        Van_Emde_Boas end = new Van_Emde_Boas(16);
 
         int choice;
         do {
@@ -448,6 +395,7 @@ class Main {
                 case 6:
                     System.out.println("Exiting...");
                     break;
+
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
